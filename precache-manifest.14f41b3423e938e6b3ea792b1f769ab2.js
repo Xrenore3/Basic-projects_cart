218dc44606f081625de632c44b9b077308b7e2e4
Xrenore3 <romanchaus519@gmail.com>
2020-12-24 19:51:00 +0200

    Updates

---
+++ precache-manifest.14f41b3423e938e6b3ea792b1f769ab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de460414ea196989e28a6415dec020d",
+    "url": "/Basic-projects_cart/index.html"
+  },
+  {
+    "revision": "70d573acd2ee59f1dc0b",
+    "url": "/Basic-projects_cart/static/css/main.8db4b5e8.chunk.css"
+  },
+  {
+    "revision": "c6816e18c4f414f7b41d",
+    "url": "/Basic-projects_cart/static/js/2.bf23568a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Basic-projects_cart/static/js/2.bf23568a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d573acd2ee59f1dc0b",
+    "url": "/Basic-projects_cart/static/js/main.49c67a16.chunk.js"
+  },
+  {
+    "revision": "b6185b374576238eb49a",
+    "url": "/Basic-projects_cart/static/js/runtime-main.52a25a1b.js"
+  }
+]);
